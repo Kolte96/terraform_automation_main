2928a4208114f0afb8d0021022dffd6c1d64521b
Kolte96 <90090813+Kolte96@users.noreply.github.com>
2023-08-18 23:46:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform"
+    bucket = "mydev-tf-state-bucket-project-terraform-gk"
     key = "main"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
